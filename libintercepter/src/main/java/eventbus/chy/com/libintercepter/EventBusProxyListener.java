774c0ee2b010df10ab7bf89d548f62dbb26bf9ca
chenhaoyang <chenhaoyang@kkworld.com>
2018-08-25 11:47:12 +0800

    event加入@NonNull

--- libintercepter/src/main/java/eventbus/chy/com/libintercepter/EventBusProxyListener.java
+++ libintercepter/src/main/java/eventbus/chy/com/libintercepter/EventBusProxyListener.java
@@ -1,12 +1,16 @@
 package eventbus.chy.com.libintercepter;
 
+import android.support.annotation.NonNull;
+
 public interface EventBusProxyListener {
     void register(EventBusProxy eventBusProxy, Object subscriber);
 
+    @NonNull
     Object beforePost(EventBusProxy eventBusProxy, Object event);
 
     void afterPost(EventBusProxy eventBusProxy, Object event);
 
+    @NonNull
     Object beforePostSticky(EventBusProxy eventBusProxy, Object event);
 
     void afterPostSticky(EventBusProxy eventBusProxy, Object event);
